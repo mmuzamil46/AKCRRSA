686c6949b8fdf20fc471e7afa755a289c5b957f6
muhammed <ptype33@gmail.com>
2024-06-26 18:41:45 +0300

    footer

--- main.js
+++ main.js
@@ -12,3 +12,17 @@ const showMenu = (toggleId, navId) =>{
  }
  
  showMenu('nav-toggle','nav-menu')
+ const tabs = document.querySelectorAll('.tab_btn');
+ const all_content = document.querySelectorAll('.contant');
+
+ tabs.forEach((tab, index)=>{
+    tab.addEventListener('click', (e)=>{
+        tabs.forEach(tab=>{tab.classList.remove('active')});
+        tab.classList.add('active');
+        // var line=document.querySelector('.line');
+        // line.style.width = e.target.offsetwidth + "px";
+        // line.style.left = e.target.offsetleft + "px";
+        all_content.forEach(content=>{content.classList.remove('active')});
+        all_content[index].classList.add('active');
+    });
+ });
